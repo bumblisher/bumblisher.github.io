c9e387984ff467122c5c24ceda340c4c2f5470e4
Jin <kjb6558@naver.com>
2021-02-01 01:05:25 +0900

    모바일 커밋

--- nurse/mobile/resource/js/common.js
+++ nurse/mobile/resource/js/common.js
@@ -20,7 +20,44 @@ $(document).ready(function(){
                 $('body').css("overflow", "scroll");
             }
         });
+
+        $(".dep2.ele > li > a").on("click", function(){
+            if($(this).hasClass("on")){
+                $(this).removeClass("on");
+            }else{
+                $(this).addClass("on");
+            }
+        });
     }
     allmenu();
     
+    function accordion(){
+        var $acc = $(".accordion > ul > li");
+
+        $acc.find("a").on("click", function(){
+            var $acclist = $(this).parent();
+            if($acclist.hasClass("on")){
+                $acclist.removeClass("on");
+                $(this).next().stop().slideUp();
+            }else{
+                $acclist.addClass("on");
+                $(this).next().stop().slideDown();
+            }
+        });
+    }
+    accordion();
+
+    function tab(){
+        var $tab = $(".tab ul li a");
+
+        $tab.on("click", function(){
+            
+            var idx = $(this).parent().index();
+
+            $(".tab ul li").eq(idx).addClass("on").siblings().removeClass("on");
+            $(".tab_cont > div").eq(idx).addClass("on").siblings().removeClass("on");
+        });
+    }
+    tab();
+
 });
