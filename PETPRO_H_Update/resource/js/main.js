09c0e56566a59056e032499c757ecb47e068f97d
Bum <kjb6558@naver.com>
2023-07-26 16:03:18 +0900

    main2 중간

--- PETPRO_H_Update/resource/js/main.js
+++ PETPRO_H_Update/resource/js/main.js
@@ -1,10 +1,20 @@
 $(function(){
-
 	// 햄버거 메뉴
 	$(".btn_menu").on("click", function(){
         $(this).toggleClass("on");
 		$("header").toggleClass("active");
     }); 
+
+	$(".tab_btn button").on("click", function(){
+		var idx = $(this).parent().index();
+
+		console.log(idx);
+		$(".tab_btn button").removeClass("on");
+		$(this).addClass("on");
+		$(".tab_cont > li").removeClass("on");
+		$(".tab_cont > li").eq(idx).addClass("on");
+	});
+	
 });
 
 
