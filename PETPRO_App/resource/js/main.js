9c8c8dabb5ea067a0510910c28db5adddc2e2ca4
bum <kjb6558@naver.com>
2023-10-22 02:32:03 +0900

    동물병원

--- PETPRO_App/resource/js/main.js
+++ PETPRO_App/resource/js/main.js
@@ -1,17 +1,7 @@
 $(function(){
-	// 햄버거 메뉴
-	$(".btn_menu").on("click", function(){
-        $(this).toggleClass("on");
-		$("header").toggleClass("active");
-    }); 
-
-	$(".tab_btn button").on("click", function(){
-		var idx = $(this).parent().index();
-
-		$(this).parents(".tab_box").find(".tab_btn button").removeClass("on");
-		$(this).addClass("on");
-		$(this).parents(".tab_box").find(".tab_cont > li").removeClass("on");
-		$(this).parents(".tab_box").find(".tab_cont > li").eq(idx).addClass("on");
+	$(".list_acco button").on("click", function(){
+		$(this).parent("li").toggleClass("on");
+		$(this).find(".cont").slideToggle(300)
 	});
 	
 });
